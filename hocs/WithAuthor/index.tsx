8011992ec76f4b6c75622e5b5df0dcf802a07867
DESKTOP-5SRASK1\User <manhnh.ous@mcredit.com.vn>
2023-04-19 08:59:37 +0700

    withAuthor wapper

--- hocs/WithAuthor/index.tsx
+++ hocs/WithAuthor/index.tsx
@@ -1,17 +1,22 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+/* eslint-disable react/display-name */
+import { useAuth } from "@/context/authorContext";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 
-const WithAuthor = ({ Component, pageProps }: any) => {
-  const isAuthor = false;
-  // const router = useRouter();
+const WithAuthor = (Component: any) => {
+  return (props: any) => {
+    const [controller] = useAuth();
+    const router = useRouter();
+    const isAuthor = useMemo(() => controller.username, [controller]);
 
-  // useEffect(() => {
-  //   if (!isAuthor) {
-  //     router.push("/login");
-  //   }
-  // }, []);
-
-  return <Component {...pageProps} />;
+    useEffect(() => {
+      if (!isAuthor) {
+        router.push("/login");
+      }
+    }, []);
+    return <Component {...props} />;
+  };
 };
 
 export default WithAuthor;
--- pages/private/index.tsx
+++ pages/private/index.tsx
@@ -1,15 +1,6 @@
-import { useAuth } from "@/context/authorContext";
 import WithAuthor from "@/hocs/WithAuthor";
-import { useRouter } from "next/router";
-import React, { useEffect } from "react";
 const Private = () => {
-  const [controller, dispatch] = useAuth();
-  const router = useRouter();
-  console.log(controller, "controller");
-  useEffect(() => {
-    if (!controller.username) router.push("/login");
-  }, []);
   return <div>Private</div>;
 };
 
-export default Private;
+export default WithAuthor(Private);
