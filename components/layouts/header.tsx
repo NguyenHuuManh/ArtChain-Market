9bdc3a778cb0fd6c4db2d6ff8f21c92ab6b9731c
manhnh.ous <manhnh.ous@mcredit.com.vn>
2023-08-25 18:07:27 +0700

    fix header active

--- components/layouts/header.tsx
+++ components/layouts/header.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 import { dispatchConnect, useWallet } from "@/context/walletContext";
 import { ethers, verifyMessage } from "ethers";
 import Link from "next/link";
@@ -18,6 +19,9 @@ const Header = () => {
   const [open, setOpen] = useState(false);
   const anchorRef = useRef<HTMLButtonElement>(null);
   const [isMobile, setIsMobile] = useState(false);
+  const routerName = router.pathname;
+  console.log(routerName, '====routerName===');
+
 
   const createConnection = async (address?: string, reConnect?: boolean) => {
     const provider = await new ethers.BrowserProvider(window?.ethereum);
@@ -30,7 +34,7 @@ const Header = () => {
     }
     const status = signer ? 'CONNECTED' : 'NOT_CONNECTED';
     dispatchConnect(dispatch, { provider, signer, status });
-    if (status === 'CONNECTED') router.push('/')
+    return status;
   }
 
   useEffect(() => {
@@ -78,6 +82,10 @@ const Header = () => {
     setOpen((prevOpen) => !prevOpen);
   };
 
+  const menu = [
+    { path: '/', name: "Market place", icon: <StoreIcon /> },
+    { path: '/collections', name: "Collections", icon: <CollectionsBookmarkIcon /> },
+  ]
 
   return (
     <div className="header">
@@ -107,8 +115,11 @@ const Header = () => {
             placeholder="Search NFT..."
             style={{ maxWidth: 200 }}
           />
-          <Link className="logo-link" href="/"><StoreIcon style={{ marginRight: 10 }} />Market place</Link>
-          <Link className="logo-link" href="/collections"><CollectionsBookmarkIcon style={{ marginRight: 10 }} /> Collections</Link>
+          {menu.map((e) =>
+            <Link key={e.path}
+              className="logo-link" href={e.path} style={{ color: routerName == e.path ? "yellow" : 'white' }}>
+              {e.icon}{e.name}
+            </Link>)}
         </>
       )}
 
@@ -117,7 +128,9 @@ const Header = () => {
           <WalletSelect />
         </div>
         :
-        <div className="item-link logo-link"><button onClick={() => createConnection()}>
+        <div className="item-link logo-link"><button onClick={() => createConnection().then((res) => {
+          if (res === "CONNECTED") router.push('/');
+        })}>
           <LoginIcon style={{ marginRight: 10 }} />
           Connect to wallet
         </button></div>
@@ -147,8 +160,12 @@ const Header = () => {
                   onKeyDown={handleListKeyDown}
                 >
                   <MenuItem onClick={handleClose}> <Link className="logo-link" href="/">ART-CHAIN.MARKET</Link></MenuItem>
-                  <MenuItem onClick={handleClose}><Link className="logo-link" href="/"><StoreIcon style={{ marginRight: 10 }} />Market place</Link></MenuItem>
-                  <MenuItem onClick={handleClose}><Link className="logo-link" href="/collections"><CollectionsBookmarkIcon style={{ marginRight: 10 }} /> Collections</Link></MenuItem>
+                  {menu.map((e) =>
+                    <Link key={e.path} className="logo-link" href={e.path} style={{ color: routerName == e.path ? "yellow" : 'white' }}>
+                      <MenuItem onClick={handleClose}>
+                        <Link className="logo-link" href={e.path}>{e.icon}{e.name}</Link>
+                      </MenuItem>
+                    </Link>)}
                 </MenuList>
               </ClickAwayListener>
             </Paper>
